5aa5e3130e2d9fd7ca47bf1298cd5d8b5ad5051f
Alexander Moiseev <sobergrim@gmail.com>
2021-12-19 00:34:11 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Linkedin Learning courses Downloader
+Linkedin Learning courses Downloader chrome plugin
+
+www.linkedin.com/learning has a lot of interesting learning courses, wich you would possibly prefer to download for further watching.
+Sadly, there is no "download this video course" button.
+
+This plugin solves the problem. Whenever you open a Linkedin Learning courses page with a video - this video auto downloads to your "Downloads" folder.
+Example: you open "Learning Java" course, "What is Java?" video is auto started. The same time you are watching the video the plugin auto-downloads it with a name "Learning_Java_-_What_is_Java_.mp4" to
+your "Downloads" folder. When the next video "Downloading Java on your computer" auto starts - the plugin auto-downloads it to your "Downloads" folder too. And so on.
+
+
+# How to:
+1* Install in chrome (Plugins -> Developer mode -> Install unpacked plugin -> Choose folder with this plugin (it will be shown as empty). Press OK.<br>
+2* Open www.linkedin.com/learning -> any course (you may open several courses in different tabs at the same time - it is okey too)<br>
+3* Plugin starts auto-downloading any videos to your "Downloads" folder in background. Noting nore needed for it to work.
+4* ...<br>
+5* ...Watch your saved videos any time later or share with friends...<br>
+5* PROFIT<br>
+
+<b>Have Fun!<b>
+
---
+++ myscript.js
@@ -0,0 +1,54 @@
+﻿function downloadFile(urlToSend, fileName) {
+    let myFileNme = fileName;
+    console.log("Downloading: " + myFileNme);
+
+    var req = new XMLHttpRequest();
+    req.open("GET", urlToSend, true);
+    req.responseType = "blob";
+    req.onload = function (event) {
+        var blob = req.response;
+        var link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = myFileNme;
+        link.click();
+    };
+
+    req.send();
+}
+
+
+function parseVideoUrlandDownload() {
+    let videos = document.getElementsByClassName("vjs-tech");
+
+    let courseName = document.getElementsByClassName("clamp-1 t-16");
+    var title1 = courseName[0].textContent.trim().replace(':', '#').replaceAll(' ', '_');
+    var title2 = courseName[1].textContent.trim().replace(':', '#').replaceAll(' ', '_');
+
+    var fileName = (title1 + "_-_" + title2 + ".mp4");
+    downloadFile(videos[0].getAttribute("src"), fileName);
+}
+
+function monitorURLChange() {
+    let oldlocation = window.location.pathname;
+    console.log("URL monitoring started");
+
+    var detect = function () {
+        if (oldlocation != window.location.pathname) {
+            console.log("Url chenged to " + window.location.pathname);
+            oldlocation = window.location.pathname;
+
+            parseVideoUrlandDownload();
+        }
+    };
+    this.Check = setInterval(function () { detect() }, 2000);
+}
+
+
+{
+
+    console.log("Linkedin Learning courses Downloader script Started");
+    setTimeout(function () { parseVideoUrlandDownload() }, 5000);
+
+    var monitor = new monitorURLChange();
+
+};
